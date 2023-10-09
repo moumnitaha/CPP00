1a906b0e52c966a9a163ada4dc283261942cbda1
Taha Moumni <53491043+moumnitaha@users.noreply.github.com>
2023-10-09 14:26:55 +0100

    ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: tmoumni <tmoumni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 16:21:08 by tmoumni           #+#    #+#             */
-/*   Updated: 2023/09/16 11:39:56 by tmoumni          ###   ########.fr       */
+/*   Updated: 2023/10/09 14:20:31 by tmoumni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@ int main(int ac, char **av)
 {
 	size_t i = 0;
 	int j = 1;
+	std::string str;
 	
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	while(j < ac)
 	{
+		str = av[j];
 		i = 0;
-		while(i < strlen(av[j]))
+		while(i < str.length())
 		{
-			std::cout << (char)std::toupper(av[j][i]);
+			std::cout << (char)std::toupper(str[i]);
 			i++;
 		}
-		if (av[j + 1])
-			std::cout << " ";
 		j++;
 	}
 	std::cout << std::endl;
