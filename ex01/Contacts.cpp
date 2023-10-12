90c043a48bf7ff37e9e35838165bb06cd9e4a37c
Taha Moumni <53491043+moumnitaha@users.noreply.github.com>
2023-10-12 13:45:30 +0100

    ex01

--- ex01/Contacts.cpp
+++ ex01/Contacts.cpp
@@ -6,7 +6,7 @@
 /*   By: tmoumni <tmoumni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 14:21:59 by tmoumni           #+#    #+#             */
-/*   Updated: 2023/10/08 15:08:47 by tmoumni          ###   ########.fr       */
+/*   Updated: 2023/10/12 12:18:06 by tmoumni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,29 +22,29 @@ Contacts::~Contacts()
 	
 }
 
-void Contacts::set_first_name(std::string first_name)
+void Contacts::set_first_name(std::string f_n)
 {
-	this->first_name = first_name;
+	this->first_name = f_n;
 }
 
-void Contacts::set_last_name(std::string last_name)
+void Contacts::set_last_name(std::string l_n)
 {
-	this->last_name = last_name;
+	this->last_name = l_n;
 }
 
-void Contacts::set_nick_name(std::string nick_name)
+void Contacts::set_nick_name(std::string n_n)
 {
-	this->nick_name = nick_name;
+	this->nick_name = n_n;
 }
 
-void Contacts::set_phone_nbr(std::string phone_nbr)
+void Contacts::set_phone_nbr(std::string p_n)
 {
-	this->phone_nbr = phone_nbr;
+	this->phone_nbr = p_n;
 }
 
-void Contacts::set_darkest_secret(std::string darkest_secret)
+void Contacts::set_darkest_secret(std::string d_s)
 {
-	this->darkest_secret = darkest_secret;
+	this->darkest_secret = d_s;
 }
 
 std::string Contacts::get_first_name(void) const
